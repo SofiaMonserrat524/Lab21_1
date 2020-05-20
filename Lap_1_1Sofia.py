c2e2fa9ad412d6a174424ff0271d69abcf62920c
SOFIA QUIROZ <SofiaMonserrat524@hotmail.com>
2020-05-20 15:15:11 -0500

    Agregue mis pasatiempos

---
+++ Lap_1_1Sofia.py
@@ -0,0 +1,7 @@
+class pasatiemposSofia ():
+    def __init (self,jugar,ejercicio,hacerComida):
+        self.jugar=jugar
+        self.ejercicio=ejercicio
+        self.hacerComida=hacerComida
+    def imprimirPasatiempos (self):
+        print (f"Mis pasatiempos son :{self.jugar},{self.ejercicio},{self.hacerComida}")
